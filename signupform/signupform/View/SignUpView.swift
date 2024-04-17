7522d556950372ce0e87d3f95f05a7ee9c7bbd9f
gadielpaiz <gadiel.paiz@gmail.com>
2024-04-17 17:49:20 -0600

    SUFD-7: Done
    Task Complete

--- signupform/signupform/View/SignUpView.swift
+++ signupform/signupform/View/SignUpView.swift
@@ -12,87 +12,91 @@ struct SignUpView: View {
     
     
     var body: some View {
-        VStack {
-            Spacer()
+        ZStack {
+            Backgroud(colorLeading: .blue, colorTrailing: .red, startPoint: .bottomLeading, endPoint: .topTrailing)
             
-            Text("Sign Up")
-                .font(.system(.title, design: .rounded, weight: .semibold))
-                .padding(.vertical)
-            
-            HStack {
+            VStack {
                 Spacer()
                 
-                VStack {
-                    TextField("Full name", text: $signUpViewModel.fullName )
-                        .offset(y: 8)
-                        .padding(.vertical, 10)
-                    
-                    if let fullNameMessage = signUpViewModel.validationMessage["fullname"] {
-                        Text(fullNameMessage)
-                            .foregroundStyle(.red)
-                            .offset(y: 8)
-                            .padding(.top, 10)
-                    } else {
-                        Text("")
-                            .padding(.vertical, 10)
-                    }
-                    
-                    TextField("Email", text: $signUpViewModel.email )
-                        .keyboardType(.emailAddress)
-                        .offset(y: 8)
-                        .padding(.vertical, 10)
-                    
-                    if let emailMessage = signUpViewModel.validationMessage["email"] {
-                        Text(emailMessage)
-                            .foregroundStyle(.red)
-                            .offset(y: 8)
-                            .padding(.top, 10)
-                    }  else {
-                        Text("")
-                            .padding(.vertical, 10)
-                    }
-                    
-                    SecureField("Password", text: $signUpViewModel.password )
-                        .offset(y: 8)
-                        .padding(.vertical, 10)
-                    
-                    if let passwordMessage = signUpViewModel.validationMessage["password"] {
-                        Text(passwordMessage)
-                            .foregroundStyle(.red)
-                            .offset(y: 8)
-                            .padding(.top, 10)
-                    }  else {
-                        Text("")
-                            .padding(.vertical, 10)
-                    }
-                    
-                    
-                    Button("Submit") {
-                        signUpViewModel.submitValidation()
-                    }
-                    .buttonStyle(BorderedButtonStyle())
-                    .padding(.vertical, 12)
-                    
-                    if let validMessage = signUpViewModel.validationMessage["Validation"] {
-                        Text(validMessage)
-                            .foregroundStyle(.green)
-                            .font(.headline)
-                            .offset(y: 8)
-                            .padding(.top, 1)
-                    }  else {
-                        Text("")
-                            .padding(.top, 1)
-                    }
-                    
-                }
-                .multilineTextAlignment(.center)
+                Text("Sign Up")
+                    .font(.system(.title, design: .rounded, weight: .semibold))
+                    .padding(.vertical)
                 
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        TextField("Full name", text: $signUpViewModel.fullName )
+                            .offset(y: 8)
+                            .padding(.vertical, 10)
+                        
+                        if let fullNameMessage = signUpViewModel.validationMessage["fullname"] {
+                            Text(fullNameMessage)
+                                .foregroundStyle(.red)
+                                .offset(y: 8)
+                                .padding(.top, 10)
+                        } else {
+                            Text("")
+                                .padding(.vertical, 10)
+                        }
+                        
+                        TextField("Email", text: $signUpViewModel.email )
+                            .keyboardType(.emailAddress)
+                            .offset(y: 8)
+                            .padding(.vertical, 10)
+                        
+                        if let emailMessage = signUpViewModel.validationMessage["email"] {
+                            Text(emailMessage)
+                                .foregroundStyle(.red)
+                                .offset(y: 8)
+                                .padding(.top, 10)
+                        }  else {
+                            Text("")
+                                .padding(.vertical, 10)
+                        }
+                        
+                        SecureField("Password", text: $signUpViewModel.password )
+                            .offset(y: 8)
+                            .padding(.vertical, 10)
+                        
+                        if let passwordMessage = signUpViewModel.validationMessage["password"] {
+                            Text(passwordMessage)
+                                .foregroundStyle(.red)
+                                .offset(y: 8)
+                                .padding(.top, 10)
+                        }  else {
+                            Text("")
+                                .padding(.vertical, 10)
+                        }
+                        
+                        
+                        Button("Submit") {
+                            signUpViewModel.submitValidation()
+                        }
+                        .buttonStyle(BorderedButtonStyle())
+                        .padding(.vertical, 12)
+                        
+                        if let validMessage = signUpViewModel.validationMessage["Validation"] {
+                            Text(validMessage)
+                                .foregroundStyle(.green)
+                                .font(.headline)
+                                .offset(y: 8)
+                                .padding(.top, 1)
+                        }  else {
+                            Text("")
+                                .padding(.top, 1)
+                        }
+                        
+                    }
+                    .multilineTextAlignment(.center)
+                    
+                    Spacer()
+                }
                 Spacer()
             }
-            Spacer()
-        }
-        .onAppear {
-            AnalitycsService.currentScreenView("Sign Up")
+            .onAppear {
+                AnalitycsService.currentScreenView("Sign Up")
+            }
         }
     }
 }
