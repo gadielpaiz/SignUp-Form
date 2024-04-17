e1ebfb129198c9a79043ae0016f15680f45321c6
gadielpaiz <gadiel.paiz@gmail.com>
2024-04-17 12:04:17 -0600

    SUFD-3: Done
    Created everything you needed for SignUpView, additionally created a button to make validation.

---
+++ signupform/signupform/View/SignUpView.swift
@@ -0,0 +1,43 @@
+//
+//  SignUpView.swift
+//  signupform
+//
+//  Created by Gadiel Paiz on 4/17/24.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    @State private var fullname = ""
+    @State private var email = ""
+    @State private var password = ""
+    
+    
+    var body: some View {
+        VStack {
+            
+            Text("Sign Up")
+                .font(.system(.title, design: .rounded, weight: .semibold))
+                .padding(.top)
+            HStack {
+                Spacer()
+                
+                VStack {
+                    TextField("Full name", text: $fullname )
+                    TextField("Email", text: $email )
+                    SecureField("Password", text: $password )
+                    
+                    Button("Submit") {
+                        
+                    }
+                }
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    SignUpView()
+}
--- signupform/signupform/signupformApp.swift
+++ signupform/signupform/signupformApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct signupformApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SignUpView()
         }
     }
 }
